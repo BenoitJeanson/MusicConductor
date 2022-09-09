9e10845925ce9ee528d17dca6ca37ca4629389dd
BenoitJeanson <benoit.jeanson@rte-france.com>
2022-09-09 11:16:27 +0200

    packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(name='musicconductor',
+version='0.1',
+description='Music conductor designer',
+url='#',
+author='Benoît Jeanson',
+                 install_requires=['typing', 'yattag', 'importlib'],
+author_email='',
+packages=setuptools.find_packages(),
+include_package_data=True,
+zip_safe=False)
